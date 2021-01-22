56b326abb6c5af37c0c69a7d06ee2939a28b4bb3
suhailaboulabed <77784903+suhailaboulabed@users.noreply.github.com>
2021-01-22 23:14:33 +0200

    add job templates

--- job/models.py
+++ job/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
 
 JOB_TYPE = (
     ('Full Time','Full Time'),
     ('Part Time','Part Time'),
 )
+def image_upload(instance,filename):
+    imagename , extension = filename.split(".")
+    return "jobs/%s.%s"%(instance.id,extension) 
+
 
 class Job(models.Model):
     title = models.CharField(max_length=1000000000000000000000000000000000000)
@@ -16,6 +21,7 @@ class Job(models.Model):
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_upload)
     
 
 
--- project/settings.py
+++ project/settings.py
@@ -126,3 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
